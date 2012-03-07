68da1d0cfc007986c79828e92c347c148fb169d7
Robert Nordan <rpvn@robpvn.net>
2012-03-07 11:38:20 +0100

    Made the post-load redraw work, changes colour

--- static/globalsearch_general/worldtiles.js
+++ static/globalsearch_general/worldtiles.js
@@ -30,19 +30,22 @@ map.add(geoJson_layer);
 
 map.add(po.compass()
     .pan("none"));
-    
+
+var layer_container = document.getElementById("org.polymaps.1").parentNode;  
 
   function load(e) {
   for (var i = 0; i < e.features.length; i++) {
     var feature = e.features[i].data, d = feature.properties.UN;
     //console.log (feature.properties.NAME + " NR: " + d);
-    e.features[i].element.setAttribute("class", "country"); //Could probably be done better
+    //e.features[i].element.setAttribute("class", "country"); //Could probably be done better
+    e.features[i].element.setAttribute("fill", "blue");
      e.features[i].element.setAttribute("UN_code", d);
   }
   
   loaded_tiles++;
   if (loaded_tiles == load_target) {
   	console.log("load target reached");
+  	ConcatenateTiles ();
   	
   }
   
@@ -58,6 +61,25 @@ map.add(po.compass()
   	loaded_tiles = 0;
   }
   
+}
+
+function ConcatenateTiles () {
+	//var layer_container = document.getElementById("org.polymaps.1").parentNode;
+	var tiles = layer_container.lastChild.children;
+	var tile;
+	var segment;
+	console.log ("Found tiles");
+	for (var i = 0; i < tiles.length; i++) {
+	console.log ("tile");
+	tile = tiles[i];
+		
+		for (var j = 0; j < tile.children.length; j++) {
+			segment = tile.children[j]
+			console.log ("fragments");
+			console.log (segment.getAttributeNS(null,"UN_code"));
+			segment.setAttribute("fill", "lightblue");
+		}
+	}
 }
 
 
