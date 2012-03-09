7e9a6b32c9c2eeb0770714eb9e0eebd85811c9f4
Robert Nordan <rpvn@robpvn.net>
2012-03-09 11:57:24 +0100

    Rudimentary concat working
    Includes primitive functionality that can be used for other examples.
    Still a (fairly large) number of bugs, such as some countries not being
    concatanated and the concat trigger being VERY buggy. Also, visual
    concat on hover is by CSS, actually changing the border lines is put
    off.(So no proper proper union of SVGs yet, can be done if time...)

--- static/globalsearch_general/worldtiles.js
+++ static/globalsearch_general/worldtiles.js
@@ -30,6 +30,9 @@ map.add(geoJson_layer);
 
 map.add(po.compass()
     .pan("none"));
+    
+//TEST
+
 
 var layer_container = document.getElementById("org.polymaps.1").parentNode;  
 
@@ -37,7 +40,7 @@ var layer_container = document.getElementById("org.polymaps.1").parentNode;
   for (var i = 0; i < e.features.length; i++) {
     var feature = e.features[i].data, d = feature.properties.UN;
     //console.log (feature.properties.NAME + " NR: " + d);
-    //e.features[i].element.setAttribute("class", "country"); //Could probably be done better
+    e.features[i].element.setAttribute("class", "country"); //Could probably be done better
     e.features[i].element.setAttribute("fill", "blue");
      e.features[i].element.setAttribute("UN_code", d);
   }
@@ -64,22 +67,98 @@ var layer_container = document.getElementById("org.polymaps.1").parentNode;
 }
 
 function ConcatenateTiles () {
+
+	/*var clippingrect = document.getElementById ("org.polymaps.1");
+	clippingrect.firstChild.setAttribute ("width", 2048);
+	clippingrect.firstChild.setAttribute ("height", 2048);*/
+
 	//var layer_container = document.getElementById("org.polymaps.1").parentNode;
 	var tiles = layer_container.lastChild.children;
 	var tile;
 	var segment;
-	console.log ("Found tiles");
+	var offsets;
+	var id;
+	//console.log ("Found tiles");
+	
+	var completedFeatures = new Array();
+	var tileSegments;
+	
 	for (var i = 0; i < tiles.length; i++) {
-	console.log ("tile");
+	
+	
+	
+	//console.log ("tile");
 	tile = tiles[i];
-		
+	
+	//Preprare the tile for larger features
+	
+	tile.removeAttribute ("clip-path");
+	
+	offsets = FindTileOffset (tile);
 		for (var j = 0; j < tile.children.length; j++) {
 			segment = tile.children[j]
-			console.log ("fragments");
-			console.log (segment.getAttributeNS(null,"UN_code"));
+			//console.log ("fragments");
+			//console.log (segment.getAttributeNS(null,"UN_code"));
+			
+			//Getting the unique ID
+			id = segment.getAttribute("UN_code");
+			if (id in completedFeatures) continue;
+			
+			tileSegments = [segment];
+			
+			for (var k = i+1; k < tiles.length; k++) {
+				for (var l = 0; l < tiles[k].children.length; l++) {
+					if (tiles[k].children[l].getAttribute("UN_code") == id) {
+						tileSegments.push (tiles[k].children[l]);
+					}
+				}
+			}
+			
+			for (var m = 1; m <tileSegments.length; m++) {
+				segment.setAttribute("d", segment.getAttribute("d") + TranslateCoordinates (tileSegments[m].getAttribute("d"), offsets, FindTileOffset (tileSegments[m].parentNode)));
+				tileSegments[m].parentNode.removeChild (tileSegments[m]);
+			}
+			
 			segment.setAttribute("fill", "lightblue");
+			completedFeatures.push (id);
+			//TEMP
+			//segment.setAttribute("d", segment.getAttribute("d") + TranslateCoordinates ("M100,100L200,200L200,100L100,200L100,100Z", offsets));
+			
+			
+			//TODO: ACTUALLY FINISH THE TODO! (Need to compensate bot ways)
+			//TODO: To union segments into one tile, you add the tile offset of the "source tile" and substract the tile offset of the "destination tile". You can delete the segment from the source til to avoid mutiple svgs on top of each other. (It shouldn't matter to the polymaps tile cache because it works on the tile level only.) Need to write something that parses and works with d attributes first!
 		}
 	}
+}
+
+function FindTileOffset (tile)
+{
+	var offsetText = tile.getAttribute("transform");
+	offsetText = offsetText.substring (10, offsetText.length -1); //translate(904.5,344.5)
+	//console.log (offsetText);
+	var offsets = offsetText.split(",");
+	offsets = [parseFloat (offsets[0]), parseFloat(offsets[1])];
+	return offsets;
+}
+
+function TranslateCoordinates (path, offset_dest, offset_source)
+{
+	//var test = "M1000,1000L2000,2000L2000,1000L1000,2000L1000,1000Z";
+	
+	var paintingInstructions = Raphael.parsePathString(path);
+	var result = "";
+	
+	for (var i = 0; i < paintingInstructions.length; i++)
+	{
+		if (paintingInstructions[i][0] != "Z") {
+			result += paintingInstructions[i][0] + (paintingInstructions[i][1] + offset_source[0] - offset_dest[0]) + "," + (paintingInstructions[i][2] + offset_source[1] - offset_dest[1]);
+			//result += paintingInstructions[i][0] + (paintingInstructions[i][1]  + offset_dest[0]) + "," + (paintingInstructions[i][2] + offset_dest[1]);
+		} else {
+			result += "Z";
+		}
+	}
+
+	return result;
 }
 
 
