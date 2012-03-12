300f9dda44766761db698c6353be34bdf08bb5d8
Robert Nordan <rpvn@robpvn.net>
2012-03-12 14:39:51 +0100

    Code cleanup & indentation

--- static/common_functions.js
+++ static/common_functions.js
@@ -1,13 +1,12 @@
 // Nice to have this as a common function with an eye to improving it
-// To union segments into one tile, you add the tile offset of the "source tile" and substract the tile offset of the "destination tile". You can delete the segment from the source til to avoid mutiple svgs on top of each other. (It shouldn't matter to the polymaps tile cache because it works on the tile level only.)
-function CombineSegments (dest_tile, source_tile, offsets_dest)
-{
+/* To union segments into one tile, you add the tile offset of the "source tile" and substract the tile offset of the "destination tile". 
+You can delete the segment from the source til to avoid mutiple svgs on top of each other. (It shouldn't matter to the polymaps tile cache because it works on the tile level only.) */
+function CombineSegments (dest_tile, source_tile, offsets_dest) {
 	dest_tile.setAttribute("d", dest_tile.getAttribute("d") + TranslateCoordinates (source_tile.getAttribute("d"), offsets_dest, FindTileOffset (source_tile.parentNode)));
 }
 
 //Finds the offset from the origin of each tile
-function FindTileOffset (tile)
-{
+function FindTileOffset (tile) {
 	var offsetText = tile.getAttribute("transform");
 	offsetText = offsetText.substring (10, offsetText.length -1); //translate(904.5,344.5)
 	//console.log (offsetText);
@@ -17,19 +16,16 @@ function FindTileOffset (tile)
 }
 
 //Translates the local coordinates from the source tile to make them fit in the local system of the dest tile
-function TranslateCoordinates (path, offset_dest, offset_source)
-{
-	//var test = "M1000,1000L2000,2000L2000,1000L1000,2000L1000,1000Z";
+function TranslateCoordinates (path, offset_dest, offset_source) {
 	
 	var paintingInstructions = Raphael.parsePathString(path);
 	var result = "";
 	
-	for (var i = 0; i < paintingInstructions.length; i++)
-	{
+	for (var i = 0; i < paintingInstructions.length; i++) {
 		if (paintingInstructions[i][0] != "Z") {
 			result += paintingInstructions[i][0] + (paintingInstructions[i][1] + offset_source[0] - offset_dest[0]) + "," + (paintingInstructions[i][2] + offset_source[1] - offset_dest[1]);
 		} else {
-			result += "Z";
+			result += "Z"; //Add the Z to close the drawing instructions, see W3C rules
 		}
 	}
 
@@ -37,12 +33,12 @@ function TranslateCoordinates (path, offset_dest, offset_source)
 }
 
 //Method to allow us to check if an array contains an object in stupid Javascript
-function oc(a)
-{
-  var o = {};
-  for(var i=0;i<a.length;i++)
-  {
-    o[a[i]]='';
-  }
-  return o;
+function oc(a) {
+	var o = {};
+	for (var i = 0; i < a.length; i++)
+	{
+		o[a[i]]='';
+	}
+	return o;
 }
+
--- static/globalsearch_general/worldtiles.js
+++ static/globalsearch_general/worldtiles.js
@@ -4,64 +4,60 @@ var loaded_tiles = 0;
 var load_target = 0;
 
 var map = po.map()
-    .container(document.getElementById("map").appendChild(po.svg("svg")))
-    .center({lat: 50, lon: 10})
-    .zoomRange([2, 7])
-    .zoom(5)
-    .add(po.interact());
+	.container(document.getElementById("map").appendChild(po.svg("svg")))
+	.center({lat: 50, lon: 10})
+	.zoomRange([2, 7])
+	.zoom(5)
+	.add(po.interact());
 
 
 var geoJson_layer = (po.geoJson()
-    .url("http://127.0.0.1:8080/vector_test/{Z}/{X}/{Y}.geojson") //Remember to use the ip adress rather than localhost to avoid XDomain trouble
-    .on("load", po.stylist()
-    	.title(function(d) { return d.properties.NAME + ": " + d.properties.UN + " UN"; })
-    )
-    
-    .on("load", load)
-    .on("move", loadedMap)
+	.url("http://127.0.0.1:8080/vector_test/{Z}/{X}/{Y}.geojson") //Remember to use the ip adress rather than localhost to avoid XDomain trouble
+	.on("load", po.stylist()
+		.title(function(d) { return d.properties.NAME + ": " + d.properties.UN + " UN"; })
+	)
+
+	.on("load", load)
+	.on("move", loadedMap)
 );
 	
 map.add(geoJson_layer);
 
 map.add(po.compass()
-    .pan("none"));
-    
-
-
+	.pan("none"));
 
 var layer_container = document.getElementById("org.polymaps.1").parentNode;  
 
-  function load(e) {
-  for (var i = 0; i < e.features.length; i++) {
-    var feature = e.features[i].data, d = feature.properties.UN;
-    //console.log (feature.properties.NAME + " NR: " + d);
-    e.features[i].element.setAttribute("class", "country"); //Could probably be done better
-    e.features[i].element.setAttribute("fill", "blue");
-     e.features[i].element.setAttribute("UN_code", d);
-  }
+function load(e) {
+	for (var i = 0; i < e.features.length; i++) {
+		var feature = e.features[i].data, d = feature.properties.UN;
+		//console.log (feature.properties.NAME + " NR: " + d);
+		e.features[i].element.setAttribute("class", "country"); //Could probably be done better
+		e.features[i].element.setAttribute("fill", "blue");
+		e.features[i].element.setAttribute("UN_code", d);
+	}
   
-  loaded_tiles++;
-  if (loaded_tiles == load_target) {
-  	console.log("load target reached");
-  	loaded_tiles = 0;
-  	ConcatenateTiles ();
+	loaded_tiles++;
+	if (loaded_tiles == load_target) {
+		console.log("load target reached");
+		loaded_tiles = 0;
+		ConcatenateTiles ();
   	
-  }
-  
+	}
 } 
 
-  function loadedMap(e) {
-
-  console.log("Added " + e.added);
-  console.log("Removed " + e.removed);
-  
-  if (e.added != 0) {
-  	load_target = e.added - e.removed;
-  	loaded_tiles = 0;
-  }
-  
-}
 //TODO: Make a more robust load-finished detection
+function loadedMap(e) {
+
+	console.log("Added " + e.added);
+	console.log("Removed " + e.removed);
+  
+	if (e.added != 0) {
+		load_target = e.added - e.removed;
+		loaded_tiles = 0;
+	} 
+}
+
 
 
 function ConcatenateTiles () {
