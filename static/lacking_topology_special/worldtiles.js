477652ca6592c6036758731342c39d51192d5fd8
Robert Nordan <rpvn@robpvn.net>
2012-04-23 11:52:47 +0200

    Clean up style on lacking topology examples

--- static/lacking_topology_special/worldtiles.js
+++ static/lacking_topology_special/worldtiles.js
@@ -1,39 +1,27 @@
 var po = org.polymaps;
 
 var map = po.map()
-    .container(document.getElementById("map").appendChild(po.svg("svg")))
-    .center({lat: 63.43, lon: 10.39280891418456})
-    .zoomRange([1, 20])
-    .zoom(16)
-    .add(po.interact());
-
+	.container(document.getElementById("map").appendChild(po.svg("svg")))
+	.center({lat: 63.43, lon: 10.39280891418456})
+	.zoomRange([1, 20])
+	.zoom(16)
+	.add(po.interact());
 
 map.add(po.geoJson()
-    .url("http://127.0.0.1:8080/trondheim_centre_buildings/{Z}/{X}/{Y}.geojson") //REMEMBER That cross-domain shit is out to kill you at any given time.
-    .on("load", po.stylist()
-    	//.attr("fill", "lightblue")
-    	//.attr("stroke", "lightblue")
-    	//.attr("fill", function(d) { return color(d.properties.UN).color; })
-    	.title(function(d) { return d.properties.name; })
-    )
-    
-    .on("load", load)
+	.url("http://127.0.0.1:8080/trondheim_centre_buildings/{Z}/{X}/{Y}.geojson") //Remember to use this address rather than localhost to avoid cross-domain restrictions.
+	.on("load", po.stylist()
+		.title(function(d) { return d.properties.name; })
+	)
+	.on("load", load)
 )
 
-
 map.add(po.compass()
-    .pan("none"));
+	.pan("none"));
     
-
-  function load(e) {
-  for (var i = 0; i < e.features.length; i++) {
-    var feature = e.features[i].data, d = feature.properties.UN;
-    //console.log (feature.properties.NAME + " NR: " + d);
-    e.features[i].element.setAttribute("class", "building"); //Could probably be done better
-  }
-  
-  
-  
+function load(e) {
+	for (var i = 0; i < e.features.length; i++) {
+		var feature = e.features[i].data;
+		e.features[i].element.setAttribute("class", "building");
+	} 
 }
-
 
